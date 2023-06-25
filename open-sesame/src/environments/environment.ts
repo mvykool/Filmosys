a172c17940f33f850e53e7000ad3762708a50084
maicol hernandez <maykolandres4@gmail.com>
2023-06-25 17:39:07 -0500

    added fire base and angular

---
+++ open-sesame/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'open-sesame-143d5',
+    appId: '1:264060857641:web:fd290055ea59bc3cee1b38',
+    storageBucket: 'open-sesame-143d5.appspot.com',
+    apiKey: 'AIzaSyC0THb52Xca7oxMQ1hFuRFGnFuE-SbAUpA',
+    authDomain: 'open-sesame-143d5.firebaseapp.com',
+    messagingSenderId: '264060857641',
+  },
+	production: false
+}
