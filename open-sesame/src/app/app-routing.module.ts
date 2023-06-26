f6d0e3b0247e67b94681345e3689c1ba081616a1
maicol hernandez <maykolandres4@gmail.com>
2023-06-26 05:28:39 -0500

    password logic

--- open-sesame/src/app/app-routing.module.ts
+++ open-sesame/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppListComponent } from './app-list/app-list.component';
+import { PasswordListComponent } from './password-list/password-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'sites-list', component: AppListComponent },
+  {path: 'password-list', component: PasswordListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- open-sesame/src/app/password-list/password-list.component.ts
+++ open-sesame/src/app/password-list/password-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OpenSesameService } from '../service/open-sesame.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-password-list',
@@ -7,4 +10,102 @@ import { Component } from '@angular/core';
 })
 export class PasswordListComponent {
 
+  //global variables
+  siteId !: string;
+  siteName !: string;
+  siteUrl !: string;
+  imageUrl !: string;
+
+
+  passwordList !: Observable<Array<any>>;
+
+
+  //global variables to show in the form
+  email !: string;
+  username !: string;
+  password !: string;
+  passwordId !: string;
+
+  //form state
+
+  formState: string = "Add new";
+
+  //popup
+  isSuccess = false;
+  succesMessage !: string;
+
+  showAlert(message: string){
+    this.isSuccess = true;
+    this.succesMessage = message;
+  }
+
+  constructor(private route: ActivatedRoute, private openSesame: OpenSesameService){
+
+    this.route.queryParams.subscribe((value: any) => {
+
+      this.siteId = value.id;
+      this.siteName = value.siteName;
+      this.siteUrl = value.url;
+      this.imageUrl = value.img;
+    });
+
+    this.loadPassword();
+  }
+
+  resetForm(){
+    this.email = "";
+    this.username = "";
+    this.password = "";
+    this.formState = "Add new";
+    this.passwordId = "";
+  }
+
+  onSubmit(value: object){
+    
+    if(this.formState == 'Add new'){
+      this.openSesame.addPassword(value, this.siteId)
+    .then(() => {
+      this.showAlert("Data Saved!");
+      this.resetForm();
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    }
+    else if(this.formState == "Edit"){
+      this.openSesame.updatePassword(this.siteId, this.password, value)
+      .then(() => {
+        this.showAlert("Data edited!");
+        this.resetForm();
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+  }
+
+  loadPassword(){
+    this.passwordList = this.openSesame.loadPassword(this.siteId)
+  }
+
+
+  editPassword(email: string, password: string, id: string, username: string){
+    this.passwordId = id;
+    this.password = password;
+    this.email = email;
+    this.username = username;
+
+    this.formState = "Edit";
+  }
+
+  deletePassword(passwordId: string){
+    this.openSesame.deletePassword(this.siteId, passwordId)
+    .then(() => {
+      this.showAlert("Data deleted!");
+      this.resetForm();
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
 }
