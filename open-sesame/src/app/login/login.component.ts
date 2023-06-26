b8b7522e61ae901606355fca3044a0441ba08788
maicol hernandez <maykolandres4@gmail.com>
2023-06-26 06:27:31 -0500

    added quick login

--- open-sesame/src/app/login/login.component.ts
+++ open-sesame/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OpenSesameService } from '../service/open-sesame.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  isError : boolean = false;
+
+  constructor(private openSesame: OpenSesameService, private router:Router){}
+
+  onSubmit(value: any){
+    this.openSesame.login(value.email, value.password)
+    .then(() => {
+      this.router.navigate(["/sites-list"])
+    })
+    .catch(err => {
+      console.log(err)
+      this.isError = true;
+    })
+  }
 
 }
--- open-sesame/src/app/service/open-sesame.service.ts
+++ open-sesame/src/app/service/open-sesame.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collection, addDoc, collectionData, doc, updateDoc,deleteDoc } from '@angular/fire/firestore';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpenSesameService {
 
-  constructor(private firestore: Firestore) { }
+  constructor(private firestore: Firestore, private auth: Auth) { }
 
   addSite(data: object) {
       const dbInstance = collection(this.firestore, "sites");
@@ -58,4 +59,12 @@ export class OpenSesameService {
 
     return deleteDoc(docInstance);
   }
+
+
+  //login
+
+  login(email: string, password: string){
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+
 }
