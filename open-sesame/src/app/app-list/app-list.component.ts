94c9e73fbfd7bc87575c062fe020a60963f19b9b
maicol hernandez <maykolandres4@gmail.com>
2023-06-25 19:54:39 -0500

    created edit

--- open-sesame/src/app/app-list/app-list.component.ts
+++ open-sesame/src/app/app-list/app-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OpenSesameService } from '../service/open-sesame.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-app-list',
@@ -6,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app-list.component.css']
 })
 export class AppListComponent {
+
+  //global data
+  allSites !: Observable<Array<any>>;
+
+  //values
+  siteName !: string;
+  siteUrl !: string;
+  imageUrl !: string;
+  siteId!: string;
+
+
+  //contructor, get service, and call queries
+
+  constructor(private openSesame: OpenSesameService){
+    this.loadSites();
+  }
+
+  //storing data
+
+  onSubmit(values:object){
+    console.log(values);
+
+    this.openSesame.addSite(values)
+    .then(() => {
+      console.log("data saved successfully");
+    })
+    .catch(err => {
+      console.log(err)
+    });
+  }
+
+  //getting data
+
+  loadSites(){
+    this.allSites = this.openSesame.loadSites();
+  }
+
+  //edit sites
+
+  editSite(siteName:string, siteUrl :string, imageUrl :string, id :string){
+      this.siteName = siteName;
+      this.siteUrl = siteUrl;
+      this.imageUrl = imageUrl;
+      this.siteId = id;
+  }
 
 }
