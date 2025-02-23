97f4a639849c357e423ca7925b0c0bf584c21d98
Maicol Hernandez <maykolandres4@gmail.com>
2025-02-23 12:40:56 -0500

    fixed environment typo

--- hotel-mk/src/app/app.config.ts
+++ hotel-mk/src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { environment } from '../enviroments/enviroment';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
