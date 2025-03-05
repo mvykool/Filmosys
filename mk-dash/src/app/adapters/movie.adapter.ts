6eb7a6811d9dfdf6b26f1f6f95687c35b4f592fb
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-04 23:29:45 -0500

    deleted adapter

--- mk-dash/src/app/adapters/movie.adapter.ts
+++
@@ -1,5 +0,0 @@
-export const movieAdapter = (movies: any) =>
-  //we want to map throug all the elements of the array, and transform the names to upper case
-  {
-    movies.map((movies: any) => movies.result);
-  };
