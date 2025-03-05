17819f3fdcb0221040818faab26ce7113413d80e
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-04 23:25:47 -0500

    rename adaptaer

--- mk-dash/src/app/adapters/movie.adapter.ts
+++ mk-dash/src/app/adapters/movie.adapter.ts
@@ -1,4 +1,4 @@
-export const characterAdapter = (movies: any) =>
+export const movieAdapter = (movies: any) =>
   //we want to map throug all the elements of the array, and transform the names to upper case
   {
     movies.map((movies: any) => movies.result);
