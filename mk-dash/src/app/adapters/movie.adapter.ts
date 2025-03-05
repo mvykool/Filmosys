d81bb93a7d138b7965aa368c2b46cc259b1e444e
Maicol Hernandez <maykolandres4@gmail.com>
2025-03-04 23:25:06 -0500

    created adapter

---
+++ mk-dash/src/app/adapters/movie.adapter.ts
@@ -0,0 +1,5 @@
+export const characterAdapter = (movies: any) =>
+  //we want to map throug all the elements of the array, and transform the names to upper case
+  {
+    movies.map((movies: any) => movies.result);
+  };
